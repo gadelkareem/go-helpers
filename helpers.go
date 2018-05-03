123ecc1180de3146f17e91f502ab0bb0f01795f5
Waleed Gadelkareem <gadelkareem@gmail.com>
2018-05-03 16:27:25 +0200

    add SendMail function

--- helpers.go
+++ helpers.go
@@ -2,11 +2,13 @@ package h
 
 import (
 	"crypto/md5"
+	"encoding/base64"
 	"encoding/hex"
 	"html"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
+	"net/smtp"
 	"net/url"
 	"regexp"
 	"strings"
@@ -159,4 +161,49 @@ func ReCaptcha(secret, response string) (bool, error) {
 	defer resp.Body.Close()
 	b, _ := ioutil.ReadAll(resp.Body)
 	return strings.Contains(string(b), "success\": true"), nil
+}
+
+
+//ex: SendMail("127.0.0.1:25", (&mail.Address{"from name", "from@example.com"}).String(), "Email Subject", "message body", []string{(&mail.Address{"to name", "to@example.com"}).String()})
+func SendMail(addr, from, subject, body string, to []string) error {
+	r := strings.NewReplacer("\r\n", "", "\r", "", "\n", "", "%0a", "", "%0d", "")
+
+	c, err := smtp.Dial(addr)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+	if err = c.Mail(r.Replace(from)); err != nil {
+		return err
+	}
+	for i := range to {
+		to[i] = r.Replace(to[i])
+		if err = c.Rcpt(to[i]); err != nil {
+			return err
+		}
+	}
+
+	w, err := c.Data()
+	if err != nil {
+		return err
+	}
+
+	msg := "To: " + strings.Join(to, ",") + "\r\n" +
+		"From: " + from + "\r\n" +
+		"Subject: " + subject + "\r\n" +
+		"Content-Type: text/html; charset=\"UTF-8\"\r\n" +
+		"Content-Transfer-Encoding: base64\r\n" +
+		"\r\n" + base64.StdEncoding.EncodeToString([]byte(body))
+
+	bMsg := []byte(msg)
+
+	_, err = w.Write(bMsg)
+	if err != nil {
+		return err
+	}
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+	return c.Quit()
 }
