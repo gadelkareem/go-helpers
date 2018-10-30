41654d34a3fc3717fe81c7c804147d8fa6e940ad
Waleed Gadelkareem <gadelkareem@gmail.com>
2018-10-30 21:26:00 +0100

    Add RegexReplaceAllStringFunc()

--- helpers.go
+++ helpers.go
@@ -216,3 +216,20 @@ func SendMail(addr, from, subject, body string, to []string) error {
 	}
 	return c.Quit()
 }
+
+func RegexReplaceAllStringFunc(re *regexp.Regexp, str string, repl func([]string) string) string {
+	result := ""
+	lastIndex := 0
+
+	for _, v := range re.FindAllSubmatchIndex([]byte(str), -1) {
+		groups := []string{}
+		for i := 0; i < len(v); i += 2 {
+			groups = append(groups, str[v[i]:v[i+1]])
+		}
+
+		result += str[lastIndex:v[0]] + repl(groups)
+		lastIndex = v[1]
+	}
+
+	return result + str[lastIndex:]
+}
