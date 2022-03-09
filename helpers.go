07bc3124e108539d10ccea07fa7f28548b3e61af
Waleed Gadelkareem <gadelkareem@gmail.com>
2022-03-09 21:49:28 +0100

    Update helpers.go

--- helpers.go
+++ helpers.go
@@ -10,7 +10,6 @@ import (
     "errors"
     "fmt"
     "html"
-    "io"
     "io/ioutil"
     "math/rand"
     "net"
@@ -21,7 +20,6 @@ import (
     "regexp"
     "strings"
     "sync"
-    "syscall"
     "time"
     "unicode/utf8"
 )
@@ -49,20 +47,6 @@ func RandomNumber(min int, max int) int {
 func Md5(text string) string {
     hash := md5.Sum([]byte(text))
     return hex.EncodeToString(hash[:])
-}
-
-func LiftRLimits() (rLimit syscall.Rlimit, err error) {
-    err = syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
-    rLimit.Cur = rLimit.Max
-    err = syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
-    if err != nil {
-        err = syscall.Setrlimit(syscall.RLIMIT_NOFILE, &syscall.Rlimit{Cur: 1048576, Max: rLimit.Max})
-        if err != nil {
-            return
-        }
-    }
-    err = syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
-    return
 }
 
 type NoRetryError struct {
@@ -347,40 +331,6 @@ func MapSearch(i interface{}, keys ...string) (m map[string]interface{}, b bool)
         }
     }
     return
-}
-
-type Flock struct {
-    *os.File
-}
-
-func NewFlock(path string) (*Flock, error) {
-    f, err := os.OpenFile(path, syscall.O_CREAT|syscall.O_RDWR|syscall.O_CLOEXEC, 0666)
-    if err != nil {
-        return nil, err
-    }
-
-    err = syscall.FcntlFlock(
-        f.Fd(),
-        syscall.F_SETLK,
-        &syscall.Flock_t{
-            Type:   syscall.F_WRLCK,
-            Whence: io.SeekStart,
-            Start:  0,
-            Len:    0,
-        })
-    if err != nil {
-        return nil, err
-    }
-
-    return &Flock{f}, nil
-}
-
-func (f *Flock) UnLock() error {
-    err := syscall.Flock(int(f.Fd()), syscall.LOCK_UN)
-    if err != nil {
-        return err
-    }
-    return f.Close()
 }
 
 type WaitGroupRunner struct {
